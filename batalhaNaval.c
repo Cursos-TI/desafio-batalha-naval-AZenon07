4d6061a4f0f3908f6f8654a51770c2408754d84e
AZenon07 <inhai321@gmail.com>
2025-07-10 17:57:27 +0000

    Trabalho batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,48 @@
-#include <stdio.h>
+#include <stdio.h>  // Para printf
+#include <string.h> // Para memset
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// Função auxiliar para exibir o tabuleiro 10x10
+void exibirMatriz(int linhas, int colunas, int matriz[][10]) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // Declaração do tabuleiro 10x10
+    int tabuleiro[10][10];
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    // Inicializa o tabuleiro com 0 (posições vazias)
+    memset(tabuleiro, 0, sizeof(tabuleiro));
+    
+    // Posicionamento dos quatro navios
+    // Navio 1: Vertical (coluna 3, linhas 0 a 2)
+    tabuleiro[0][3] = 3;
+    tabuleiro[1][3] = 3;
+    tabuleiro[2][3] = 3;
+    
+    // Navio 2: Horizontal (linha 5, colunas 1 a 3)
+    tabuleiro[5][1] = 3;
+    tabuleiro[5][2] = 3;
+    tabuleiro[5][3] = 3;
+    
+    // Navio 3: Diagonal (de (2,2) a (4,4))
+    tabuleiro[2][2] = 3;
+    tabuleiro[3][3] = 3;
+    tabuleiro[4][4] = 3;
+    
+    // Navio 4: Diagonal (de (6,6) a (8,4))
+    tabuleiro[6][6] = 3;
+    tabuleiro[7][5] = 3;
+    tabuleiro[8][4] = 3;
+    
+    // Exibição do tabuleiro
+    printf("Tabuleiro 10x10 (0 = vazio, 3 = navio):\n");
+    exibirMatriz(10, 10, tabuleiro);
+    
     return 0;
 }
